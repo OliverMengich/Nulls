3c95eeaffae5f78878ca327428489faaaeccde35
OliverMengich <oliverkemei2000@gmail.com>
2019-11-04 16:46:49 +0300

    Nulls

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Nullable_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<DateTime> date = null; //
+            System.Console.WriteLine(date);
+              
+
+              System.Console.WriteLine("Get Value or default" + date.GetValueOrDefault());
+              System.Console.WriteLine("Has value " +date.HasValue);
+              System.Console.WriteLine(date.Value);
+
+        }
+
+    }
+}
